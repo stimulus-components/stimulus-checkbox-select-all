ed64ffd5299b26711bf9018bb9bb54f3d26e19e7
Guillaume Briday <guillaumebriday@gmail.com>
2022-04-15 10:26:41 +0200

    Fixing external Vite config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Upgrading workflows
 - Upgrading to Node 16
 - Bump dependencies
+- Fixing external Vite config reducing package size from `2.70 KiB` to `0.50 KiB`.
 
 ## [5.0.0] - 2021-09-28
 
--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ module.exports = {
       name: 'stimulus-checkbox-select-all'
     },
     rollupOptions: {
-      external: ['stimulus'],
+      external: ['@hotwired/stimulus'],
       output: {
         globals: {
-          stimulus: 'Stimulus'
+          '@hotwired/stimulus': 'Stimulus'
         }
       }
     }
